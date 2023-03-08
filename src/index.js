089edd59fee893981bb391bbbac9c2e986bfa503
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-08 09:07:24 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const books = [
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL900_SR900,600_.jpg',
+    id: 1,
+  },
+
+  {
+    author: 'Rick Rubin',
+    title: 'The Creative Act: A Way of Being',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/A1YhM3oRFbL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+];
+
+// Create component
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
