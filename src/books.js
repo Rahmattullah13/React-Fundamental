4d2e8fed7d7f16eca0f5024c325a2cd5477ba19e
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-08 11:49:45 +0700

    refactoring the project

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Rick Rubin',
+    title: 'The Creative Act: A Way of Being',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Bessel van der Kolk M.D.',
+    title: 'The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma',
+    img: img3,
+    id: 2,
+  },
+];
+// export defaul books
