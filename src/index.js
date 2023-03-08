2688c665a54d74d64f9f9bf71cc05e070b5988c5
Rifki Rahmattullah <55880165+Rahmattullah13@users.noreply.github.com>
2023-03-08 11:53:18 +0700

    Revert "refactoring the project"

--- src/index.js
+++ src/index.js
@@ -2,21 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { books } from './books';
-import Book from './Book';
+const books = [
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL900_SR900,600_.jpg',
+    id: 1,
+  },
+
+  {
+    author: 'Rick Rubin',
+    title: 'The Creative Act: A Way of Being',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/A1YhM3oRFbL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+];
 
 // Create component
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book)
+  };
   return (
-    <>
-    <h1>Amazon Best Sellers</h1>
     <section className="booklist">
       {/* <EventExamples /> */}
-      {books.map((book, index) => {
-        return <Book {...book} key={book.id} number={index} />;
+      {books.map((book) => {
+        return <Book {...book} key={book.id} getBook={getBook}/>;
       })}
     </section>
-      </>
   );
 };
 
@@ -44,6 +58,22 @@ const BookList = () => {
 //     </section>
 //   );
 // };
+
+const Book = (props) => {
+  // console.log(props);
+  const { img, title, author, getBook, id } = props;
+  const getSingleBook = () => {
+    getBook(id)
+  }
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <button onClick={getSingleBook}>click me</button>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
